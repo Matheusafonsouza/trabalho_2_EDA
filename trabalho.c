20f46647eb9b7487bb68f4950b991b49b6f2c286
matheusafonsouza <matheusafonsouza@gmail.com>
2019-11-03 11:01:34 -0200

    comentando .c

--- trabalho.c
+++ trabalho.c
@@ -4,17 +4,21 @@
 #include <string.h>
 #include <stdbool.h>
 
+//Função para printar menu de disciplinas do aluno
 void menu_aluno(Node_aluno *node_aluno){
     char continuar, *procura;
     int opcao;
     Node_disciplina *node_disciplina;
     List_disciplina *list_disciplina;
+
+    //Caso haja uma lista de disciplinas, ela será carregada. Caso não haja ela será criada.
     if(node_aluno->head==NULL){
         list_disciplina=create_list_disciplina();
     }else{
         list_disciplina=node_aluno->head;
     }
 
+    //printa menu
     do{
         system("clear");
         printf("1. Cadastrar disciplina.\n");
@@ -25,65 +29,88 @@ void menu_aluno(Node_aluno *node_aluno){
         printf("6. Apresentar relatório geral do aluno.\n");
         printf("7. Voltar ao menu principal.\n");
 
+        //Faz seleção do menu
         scanf("%d",&opcao);
 
+        //Switch para opções  
         system("clear");
         switch(opcao){
+            //Cadastra disciplina
             case 1:
+                //Cria node disciplina e insere dados
                 node_disciplina=create_node_disciplica();
                 printf("Disciplina adicionada!\n");
+                //Insere o node na lista de disciplinas
                 push_disciplina(list_disciplina,node_disciplina);
                 getchar();
                 printf("Aperte enter para continuar.");
                 scanf("%c",&continuar);
                 break;
+            //printa lista de disciplinas
             case 2:
+                //printa lista de disciplinas
                 print_disciplinas(list_disciplina);
                 getchar();
                 printf("Aperte enter para continuar.");
                 scanf("%c",&continuar);
                 break;
+            //edita disciplina da lista de disciplinas
             case 3:
                 printf("Digite o nome da disciplina: ");
                 getchar();
+                //leitura do nome da disciplina
                 scanf("%[^\n]",procura);
+                //retorna node baseado no nome da disciplina
                 node_disciplina=search_disciplina(list_disciplina,procura);
+                //caso seja null a disciplina não existe
                 if(node_disciplina==NULL){
                 }else{
+                    //caso exista, os dados dela serão reinseridos
                     edit_disciplina(node_disciplina);
                 }
                 getchar();
                 printf("Aperte enter para continuar.\n");
                 scanf("%c",&continuar);
                 break;
+            //exclui disciplina da lista 
             case 4:
                 printf("Digite o nome da disciplina: ");
                 getchar();
+                //leitura do nome da disciplina
                 scanf("%[^\n]",procura);
+                //retorna node baseado no nome da disciplina
                 node_disciplina=search_disciplina(list_disciplina,procura);
+                //caso seja null a disciplina não existe
                 if(node_disciplina==NULL){
                 }else{
+                    //caso exista, a disciplina será excluida
                     erase_disciplina(list_disciplina,node_disciplina);
                 }
                 getchar();
                 printf("Aperte enter para continuar.\n");
                 scanf("%c",&continuar);
                 break;
+            //filtra disciplinas pela menção
             case 5:
+                //filtar disciplinas pela menção
                 filtrar_disciplinas(list_disciplina);
                 getchar();
                 printf("Aperte enter para continuar.");
                 scanf("%c",&continuar);
                 break;
+            //printa relatorio geral do aluno 
             case 6:
+                //printa relatorio geral do aluno
                 relatorio_geral(list_disciplina);
                 getchar();
                 printf("Aperte enter para continuar.\n");
                 scanf("%c",&continuar);   
                 break;
+            //retorna para o menu inicial e salva lista de disciplinas
             case 7:
                 node_aluno->head=list_disciplina;
                 break;
+            //opções inválidas
             default:
                 printf("Opção inválida! Aperte enter para voltar ao menu do aluno.\n");
                 getchar();
